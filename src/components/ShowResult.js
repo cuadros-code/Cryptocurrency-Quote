f5779be3ffd9737a3994512acca3c0400493d655
kevin david cuadros <kevindavidcuadros@gmail.com>
2020-12-16 16:54:52 -0500

    ajuste estilo ShowResult.js

--- src/components/ShowResult.js
+++ src/components/ShowResult.js
@@ -16,6 +16,22 @@ const Div = styled.div`
     text-align: center;
     padding: 5px;
     font-size: 1.2rem;
+
+    @media(max-width: 425px){
+    margin-left: 20px;
+    margin-top: 100px;
+    height: 50vh;
+    width: 70vw;
+    background-color: #38BDD0;
+    border-radius: 15px;
+    box-shadow: 3px 3px 3px 2px rgba(0, 0, 0, 0.2);
+    color: #FFF;
+    font-family: 'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif;
+    text-align: center;
+    padding: 5px;
+    font-size: 1.2rem;
+
+    }
 `
 const DivSpinner = styled.div`
     margin-left: 40px;
@@ -36,22 +52,22 @@ export const ShowResult = ({ dataResult, showSpinner }) => {
                 showSpinner
                     ?
                     <DivSpinner>
-                    <Loader
-                        type="Grid"
-                        color="#FFF"
-                        height={100}
-                        width={100}
-                        
-                    />
+                        <Loader
+                            type="Grid"
+                            color="#FFF"
+                            height={100}
+                            width={100}
+
+                        />
                     </DivSpinner>
                     :
-                    <Div> 
+                    <Div>
                         <h2>The price is : {dataResult.PRICE}</h2>
                         <h3>The highest price of the day: {dataResult.HIGHDAY} </h3>
                         <h3>The lowest price of the day: {dataResult.LOWDAY} </h3>
                         <h3>Change last 24 hours: {dataResult.CHANGE24HOUR} </h3>
                         <h3>Last update: {dataResult.LASTUPDATE} </h3>
-                     </Div>
+                    </Div>
 
             }
 
